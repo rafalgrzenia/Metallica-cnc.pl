f6167d15d2b261d0ea7c0e145068ab2e102d0b5f
Rafal Grzenia <rafalgrzenia96@gmail.com>
2022-11-24 16:36:53 +0100

    Add active state on click for navigation header in script.js

--- resources/javascript/script.js
+++ resources/javascript/script.js
@@ -1,7 +1,9 @@
 const hamburger = document.querySelector('.hamburger-menu');
+const navigationHeader = document.querySelector('.navigation-header');
 
 const hamburgerOnClick = () => {
     hamburger.classList.toggle('hamburger-active');
+    navigationHeader.classList.toggle('navigation-header-active');
 }
 
 hamburger.addEventListener('click', hamburgerOnClick);
