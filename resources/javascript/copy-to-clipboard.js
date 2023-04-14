a295805ba7162e4f62b50bb18c8b94383b0afee9
Rafal Grzenia <rafalgrzenia96@gmail.com>
2023-04-14 08:54:35 +0200

    Fix copy to clipboard function

--- resources/javascript/copy-to-clipboard.js
+++ resources/javascript/copy-to-clipboard.js
@@ -5,6 +5,16 @@
 const mail = document.querySelector(".header-contact-email");
 const popUp = document.querySelector(".copied-popup");
 
+// Functions
+
+function copyToClipBoard(text, element) {
+  navigator.clipboard
+    .writeText(text)
+    .then(() => {
+      element.style.display = "block";
+    })
+    .catch((e) => console.error(e));
+}
 
 // Events
 
@@ -12,18 +22,7 @@ mail.addEventListener("click", () => {
   const mailAdress = mail.innerText;
   copyToClipBoard(mailAdress, popUp);
   setTimeout(() => {
-    popUp.style.opacity = '0';
-  }, 800)
+    popUp.style.opacity = "0";
+  }, 800);
+  popUp.style.opacity = "1";
 });
-
-// Functions
-
-
-function copyToClipBoard(text, element) {
-    navigator.clipboard
-      .writeText(text)
-      .then(() => {
-        element.style.display = "block";  
-      })
-      .catch((e) => console.error(e));
-  }
