d7b82d0b20c2143f860f18766457ee86d1bef3ac
Rafal Grzenia <rafalgrzenia96@gmail.com>
2023-04-14 11:43:30 +0200

    Change photoswipe to min version and update index.html

--- resources/javascript/photoswipe.js
+++ resources/javascript/photoswipe.js
@@ -1,7 +1,7 @@
-import PhotoSwipeLightbox from './photoswipe/photoswipe-lightbox.esm.js';
+import PhotoSwipeLightbox from './photoswipe/photoswipe-lightbox.esm.min.js';
 const lightbox = new PhotoSwipeLightbox({
   gallery: '#my-gallery',
   children: 'a',
-  pswpModule: () => import('./photoswipe/photoswipe.esm.js')
+  pswpModule: () => import('./photoswipe/photoswipe.esm.min.js')
 });
 lightbox.init();
